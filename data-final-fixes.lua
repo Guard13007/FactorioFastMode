ac2ff1835a28a1c1027e7e3e98541b0219f59d8c
Paul Liverman III <paul.liverman.iii@gmail.com>
2017-10-31 11:11:51 -0700

    refactor

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,6 +1,6 @@
 local ceil = math.ceil
 
-for _, recipe in pairs(data.raw.recipe) do
+local mod_recipe = function(recipe)
   if recipe.ingredients then
     for _, ingredient in pairs(recipe.ingredients) do
       if ingredient.amount then
@@ -10,28 +10,12 @@ for _, recipe in pairs(data.raw.recipe) do
       end
     end
   end
-  if recipe.normal then
-    if recipe.normal.ingredients then
-      for _, ingredient in pairs(recipe.normal.ingredients) do
-        if ingredient.amount then
-          ingredient.amount = ceil(ingredient.amount / 2)
-        elseif ingredient[2] then
-          ingredient[2] = ceil(ingredient[2] / 2)
-        end
-      end
-    end
-  end
-  if recipe.expensive then
-    if recipe.expensive.ingredients then
-      for _, ingredient in pairs(recipe.expensive.ingredients) do
-        if ingredient.amount then
-          ingredient.amount = ceil(ingredient.amount / 2)
-        elseif ingredient[2] then
-          ingredient[2] = ceil(ingredient[2] / 2)
-        end
-      end
-    end
-  end
+end
+
+for _, recipe in pairs(data.raw.recipe) do
+  if recipe.ingredients then mod_recipe(recipe) end
+  if recipe.normal then mod_recipe(recipe.normal) end
+  if recipe.expensive then mod_recipe(recipe.expensive) end
 end
 
 for _, technology in pairs(data.raw.technology) do
